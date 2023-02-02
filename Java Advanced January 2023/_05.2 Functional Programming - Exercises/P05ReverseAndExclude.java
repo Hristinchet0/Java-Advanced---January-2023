d1b834117c88226d8a04f0182ab87e8d146fcaf1
Hristinchet0 <bansko.sport@gmail.com>
2023-02-02 19:02:12 +0200

    Add

---
+++ Java Advanced January 2023/_05.2 Functional Programming - Exercises/P05ReverseAndExclude.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class P05ReverseAndExclude {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(numbers);
+
+        Predicate<Integer> checkDivision = number -> number % n == 0;
+
+        numbers.removeIf(checkDivision);
+
+        numbers.forEach(num -> System.out.print(num + " "));
+
+    }
+}
