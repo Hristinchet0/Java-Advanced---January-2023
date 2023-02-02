980cdbee233a499b659825590d614b6c18908998
Hristinchet0 <bansko.sport@gmail.com>
2023-02-02 19:01:36 +0200

    Add

---
+++ Java Advanced January 2023/_05.2 Functional Programming - Exercises/P02KnightsOfHonor.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class P02KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] names = input.split("\\s+");
+
+        Consumer<String> printName = name -> System.out.println("Sir " + name);
+
+        Arrays.stream(names).forEach(printName);
+    }
+}
