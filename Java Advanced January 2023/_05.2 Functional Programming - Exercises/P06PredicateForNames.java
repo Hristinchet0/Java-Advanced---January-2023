345452445ddd5a54481677c4b5408eac3338bd3a
Hristinchet0 <bansko.sport@gmail.com>
2023-02-03 18:19:53 +0200

    Add

---
+++ Java Advanced January 2023/_05.2 Functional Programming - Exercises/P06PredicateForNames.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class P06PredicateForNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String[] names = scanner.nextLine().split("\\s+");
+
+        Predicate<String> checkLength = name -> name.length() <= n;
+
+        Arrays.stream(names).filter(checkLength).forEach(name -> System.out.println(name));
+
+
+    }
+}
