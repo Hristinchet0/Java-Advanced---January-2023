b8f554acf269df2787f7ce813441376f25a669ba
Hristinchet0 <bansko.sport@gmail.com>
2023-02-11 11:13:15 +0200

    Add

---
+++ Java Advanced January 2023/_07.1 Generics - Lab/P02GenericArrayCreator/ArrayCreator.java
@@ -0,0 +1,24 @@
+package P02GenericArrayCreator;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayCreator {
+
+    public static <T> T[] create(int length, T item) {
+        T[] array = (T[]) Array.newInstance(item.getClass(), length);
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+
+        }
+        return array;
+    }
+
+    public static <T> T[] create(Class<T> myClass, int length, T item) {
+        T[] array = (T[]) Array.newInstance(myClass, length);
+        for (int i = 0; i < length; i++) {
+            array[i] = item;
+        }
+        return array;
+    }
+}
