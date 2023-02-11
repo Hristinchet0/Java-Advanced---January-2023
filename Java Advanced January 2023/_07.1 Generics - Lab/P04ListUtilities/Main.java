121830b2522ce6ceab7e9bc957a81c0f311cdbb0
Hristinchet0 <bansko.sport@gmail.com>
2023-02-11 11:13:28 +0200

    Add

---
+++ Java Advanced January 2023/_07.1 Generics - Lab/P04ListUtilities/Main.java
@@ -0,0 +1,18 @@
+package P04ListUtilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(1000);
+        list.add(100);
+        list.add(-150);
+        list.add(2);
+        list.add(1);
+        System.out.println(ListUtils.getMax(list));
+        System.out.println(ListUtils.getMin(list));
+    }
+}
