f0a12b1a823ef3cba2816cdf2bb930c6328565b6
Hristinchet0 <bansko.sport@gmail.com>
2023-02-17 18:52:14 +0200

    Add

---
+++ Java Advanced January 2023/_07.2 Generics - Exercises/P01GenericBox/Box.java
@@ -0,0 +1,23 @@
+package P01GenericBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> elements;
+
+    public Box (){
+        this.elements = new ArrayList<>();
+    }
+    public void add(T element){
+        this.elements.add(element);
+    }
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        for (T element : elements){
+            builder.append(element.getClass().getName() + ": " + element).append("\n");
+        }
+        return builder.toString();
+    }
+}
