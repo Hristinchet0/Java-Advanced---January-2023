244d482b9f53ed77132d3f170f506e1c4cedbdb9
Hristinchet0 <bansko.sport@gmail.com>
2023-02-17 18:52:43 +0200

    Add

---
+++ Java Advanced January 2023/_07.2 Generics - Exercises/P05GenericCountMethodString/Main.java
@@ -0,0 +1,19 @@
+package P05GenericCountMethodString;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            String text = scanner.nextLine();
+            box.add(text);
+        }
+        String element = scanner.nextLine();
+        System.out.println(box.countGreaterThan(element));
+    }
+}
