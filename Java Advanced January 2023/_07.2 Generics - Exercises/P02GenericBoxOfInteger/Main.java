f72db3f18db0d4f91ad71042f76a3c2a84600356
Hristinchet0 <bansko.sport@gmail.com>
2023-02-17 18:52:21 +0200

    Add

---
+++ Java Advanced January 2023/_07.2 Generics - Exercises/P02GenericBoxOfInteger/Main.java
@@ -0,0 +1,19 @@
+package P02GenericBoxOfInteger;
+
+import P01GenericBox.Box;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<Integer> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            int text = Integer.parseInt(scanner.nextLine());
+            box.add(text);
+        }
+        System.out.println(box);
+    }
+}
