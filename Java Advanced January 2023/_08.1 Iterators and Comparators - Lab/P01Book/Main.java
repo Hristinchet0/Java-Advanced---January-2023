97eedb2b36b7e700b43fd54866ea1e124e42182b
Hristinchet0 <bansko.sport@gmail.com>
2023-02-18 13:46:43 +0200

    Add

---
+++ Java Advanced January 2023/_08.1 Iterators and Comparators - Lab/P01Book/Main.java
@@ -0,0 +1,14 @@
+package P01Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+    }
+}
