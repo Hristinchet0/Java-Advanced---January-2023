43447db79eca66ec799796c33417d0582928e7dd
Hristinchet0 <bansko.sport@gmail.com>
2023-02-18 13:46:50 +0200

    Add

---
+++ Java Advanced January 2023/_08.1 Iterators and Comparators - Lab/P02Library/Library.java
@@ -0,0 +1,36 @@
+package P02Library;
+
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    public Iterator<Book> iterator() {
+        return new LibIterator();
+    }
+
+    private class LibIterator implements Iterator<Book> {
+
+        private int counter;
+
+        public LibIterator() {
+            this.counter = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.counter < books.length;
+        }
+
+        @Override
+        public Book next() {
+            Book result = books[this.counter];
+            this.counter ++;
+            return result;
+        }
+    }
+}
