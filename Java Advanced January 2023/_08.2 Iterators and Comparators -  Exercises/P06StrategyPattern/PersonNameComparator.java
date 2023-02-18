109b2157df48d4bcf4082dcc92ca91d3391e6ecb
Hristinchet0 <bansko.sport@gmail.com>
2023-02-18 13:48:03 +0200

    Add

---
+++ Java Advanced January 2023/_08.2 Iterators and Comparators -  Exercises/P06StrategyPattern/PersonNameComparator.java
@@ -0,0 +1,16 @@
+package P06StrategyPattern;
+
+import java.util.Comparator;
+
+public class PersonNameComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person first, Person second) {
+        if(first.getName().length() == (second.getName().length())) {
+            char letterOfFirstPerson = first.getName().toLowerCase().charAt(0);
+            char letterOfSecondPerson = second.getName().toLowerCase().charAt(0);
+            return Character.compare(letterOfFirstPerson, letterOfSecondPerson);
+        }
+
+        return first.getName().length() - (second.getName().length());
+    }
+}
