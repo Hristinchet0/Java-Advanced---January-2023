c8047c738d4f9a2d9e1abe11d3301433af29179f
Hristinchet0 <bansko.sport@gmail.com>
2023-03-02 08:50:27 +0200

    Add

---
+++ Java OOP February 2023/_01.2 Working with Abstraction - Exercise/greedyTimes/Main.java
@@ -0,0 +1,33 @@
+package greedyTimes;
+
+import java.util.Scanner;
+
+/* 90/100 */
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        long capacity = Long.parseLong(scanner.nextLine());
+        Bag bag = new Bag(capacity);
+
+        String[] itemInfo = scanner.nextLine().split("\\s+");
+
+        for (int i = 0; i < itemInfo.length; i += 2) {
+
+            String itemName = itemInfo[i];
+            if (itemName.equalsIgnoreCase("Gold")) {
+                itemName = "Gold";
+            }
+            long amount = Long.parseLong(itemInfo[i + 1]);
+            try {
+                Item item = new Item(Type.parse(itemName), itemName, amount);
+                bag.addToBag(item);
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+        }
+
+        bag.printContent();
+    }
+
+}
