5de9e21679ce7df0fa649c4c150d2d43e64d5db5
Hristinchet0 <bansko.sport@gmail.com>
2023-03-09 16:45:27 +0200

    Add

---
+++ Java OOP February 2023/_02.1 Encapsulation - Lab/firstAndReverseTeam_04/Main.java
@@ -0,0 +1,27 @@
+package firstAndReverseTeam_04;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+        Team team = new Team("Black Eagles");
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person personToAdd = new Person(firstName, lastName, age, salary);
+            team.addPlayer(personToAdd);
+
+        }
+
+        System.out.printf("First team have %d players%n", team.getFirstTeam().size());
+        System.out.printf("Reserve team have %d players", team.getReserveTeam().size());
+    }
+}
