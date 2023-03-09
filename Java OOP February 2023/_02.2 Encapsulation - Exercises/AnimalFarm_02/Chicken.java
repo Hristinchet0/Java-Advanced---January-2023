8f8b1a08590051094b1fb423a9f5a5ee73b54b24
Hristinchet0 <bansko.sport@gmail.com>
2023-03-09 16:45:53 +0200

    Add

---
+++ Java OOP February 2023/_02.2 Encapsulation - Exercises/AnimalFarm_02/Chicken.java
@@ -0,0 +1,48 @@
+package AnimalFarm_02;
+
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public void setName(String name) {
+        if (name.length() >= 1 && !name.contains(" ")) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+    }
+
+    public void setAge(int age) {
+        if (age > 0 && age <= 15) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+    }
+
+    public double productPerDay() {
+        return this.calculateProductPerDay();
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, productPerDay());
+    }
+
+    private double calculateProductPerDay() {
+
+        if (this.age >= 0 && this.age <= 5) {
+            return 2;
+        } else if (this.age >= 6 && this.age <= 11) {
+            return 1;
+        } else {
+            return 0.75;
+        }
+    }
+}
