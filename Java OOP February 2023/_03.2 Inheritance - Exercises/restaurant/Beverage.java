028196a2d25e42451bc736b574e51630aac01158
Hristinchet0 <bansko.sport@gmail.com>
2023-03-14 11:52:02 +0200

    Add

---
+++ Java OOP February 2023/_03.2 Inheritance - Exercises/restaurant/Beverage.java
@@ -0,0 +1,21 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Beverage extends Product{
+    private double milliliters;
+
+
+    public Beverage(String name, BigDecimal price, double milliliters) {
+        super(name, price);
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters() {
+        return milliliters;
+    }
+
+    public void setMilliliters(double milliliters) {
+        this.milliliters = milliliters;
+    }
+}
---
+++ Java OOP February 2023/_03.2 Inheritance - Exercises/restaurant/Coffee.java
@@ -0,0 +1,22 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Coffee extends HotBeverage{
+    private static final double COFFEE_MILLILITERS = 50;
+    private static final BigDecimal COFFEE_PRICE = new BigDecimal(3.50);
+    private double caffeine;
+
+    public Coffee(String name, double caffeine) {
+        super(name, COFFEE_PRICE, COFFEE_MILLILITERS);
+        this.caffeine = caffeine;
+    }
+
+    public double getCaffeine() {
+        return caffeine;
+    }
+
+    public void setCaffeine(double caffeine) {
+        this.caffeine = caffeine;
+    }
+}
