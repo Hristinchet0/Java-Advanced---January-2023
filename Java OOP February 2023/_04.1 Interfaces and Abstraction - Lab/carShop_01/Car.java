2c60a31541ce45ec3416404793e01879b6081768
Hristinchet0 <bansko.sport@gmail.com>
2023-03-23 20:44:25 +0200

    Add

---
+++ Java OOP February 2023/_04.1 Interfaces and Abstraction - Lab/carShop_01/Car.java
@@ -0,0 +1,12 @@
+package carShop_01;
+
+public interface Car {
+
+    int TIERS = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+
+}
