afeea924dde9589a86a5af50f622532bcf12ea55
Hristinchet0 <bansko.sport@gmail.com>
2023-04-08 13:08:29 +0300

    Add

---
+++ Java OOP February 2023/Exam Prep/christmasRaces/src/main/java/christmasRaces/io/interfaces/InputReader.java
@@ -0,0 +1,7 @@
+package christmasRaces.src.main.java.christmasRaces.io.interfaces;
+
+import java.io.IOException;
+
+public interface InputReader {
+    String readLine() throws IOException;
+}
---
+++ Java OOP February 2023/Exam Prep/christmasRaces/src/main/java/christmasRaces/io/interfaces/OutputWriter.java
@@ -0,0 +1,5 @@
+package christmasRaces.src.main.java.christmasRaces.io.interfaces;
+
+public interface OutputWriter {
+    void writeLine(String text);
+}
